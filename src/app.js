ab0998608f91d66763bc540c060cd726a805fb41
Eduardo Dobay <edudobay@gmail.com>
2018-08-29 09:35:15 -0300

    Parse cabin commands

--- src/app.js
+++ src/app.js
@@ -43,10 +43,28 @@ class ElevatorPlatformCommand {
   }
 }
 
+class ElevatorCabinCommand {
+  constructor(floor) {
+    this.floor = floor
+  }
+
+  static parse(floor) {
+    return new ElevatorCabinCommand(parseFloor(floor))
+  }
+}
+
 app.get('/:floor,:direction', (req, res) => {
   const { floor, direction } = req.params
   logger.info(`Elevator requested at floor "${floor}" with direction "${direction}"`)
   const command = ElevatorPlatformCommand.parse(floor, direction)
+  logger.info('Parsed command:', command)
+  res.json({})
+})
+
+app.get('/cabin/:floor', (req, res) => {
+  const { floor } = req.params
+  logger.info(`Elevator requested to floor "${floor}" from cabin`)
+  const command = ElevatorCabinCommand.parse(floor)
   logger.info('Parsed command:', command)
   res.json({})
 })
